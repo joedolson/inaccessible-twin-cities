23af06da73c0affc0613ff3593a16d9fab405567
Joe Dolson <design@joedolson.com>
2014-06-15 00:09:27 -0500

    More updates
    Manage changes in iatc

--- searchform.php
+++ searchform.php
@@ -2,7 +2,7 @@
 <form method="get" action="<?php echo home_url(); ?>/">
 	<p>
 	<input type="text" name="s" id="s" placeholder="<?php _e( 'Search', 'accessible-twin-cities' ); ?>" value="<?php echo trim( get_search_query() ); ?>" /> 
-	<input type="submit" name="submit" value="<?php _e( 'Search', 'accessible-twin-cities' ); ?>" class="button" />
+	<input type="submit" name="submit" value="<?php _e( 'Go', 'accessible-twin-cities' ); ?>" class="button" />
 	</p>
 </form>
 </div>
---
+++ sidebar.php
@@ -0,0 +1,38 @@
+			</div>
+		</main>
+	</div> <?php // #content .content ?>
+<?php
+	$atc_options = get_option( 'atc_options' );
+?>
+<div id="sidebar" class="sidebar clear">
+	<?php apply_filters( 'atc_top_of_sidebar', '' ); ?>
+	<?php if ( is_front_page() ) {
+		$sidebars = wp_get_sidebars_widgets();
+		$home_sidebar = $sidebars['ps2'];
+		$count = count( $home_sidebar );
+		$class = " widgets-$count";
+	} else {
+		$class = " widgets";
+	}	?>
+	<div class='post-wrapper<?php echo $class; ?>'>
+	<?php 
+		/* Home sidebar displayed only on home page. Global sidebars on all other pages. */
+		if ( is_front_page() ) {
+		
+			dynamic_sidebar('Home Sidebar'); 
+			
+		} else {
+		
+			dynamic_sidebar( 'Global Sidebar - Top' ); 
+			if ( !is_page() ) { 
+				dynamic_sidebar( 'Post Sidebar' );
+			} else {
+				dynamic_sidebar( 'Page Sidebar' );
+			}
+			dynamic_sidebar( 'Global Sidebar - Bottom' );	
+			
+		}
+	?>
+	</div>
+	<?php apply_filters( 'atc_bottom_of_sidebar', '' ); ?>	
+</div>
