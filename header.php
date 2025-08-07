f64798ed7a7a474d0383162ede547a590e7dc971
Joe Dolson <design@joedolson.com>
2025-08-07 10:17:17 -0500

    Set max scale

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
 	<title><?php wp_title( ' &raquo; ', true, 'right' ); ?></title>
 	<meta http-equiv="Content-Type" content="text/html; charset=<?php bloginfo('charset'); ?>" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no; maximum-scale=1" />	
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
@@ -21,9 +21,8 @@
 			<?php wp_nav_menu( array( 'theme_location'=>'primary' ) ); ?>
 			</nav>
 		</div>
-		<?php apply_filters( 'universal_after_primary_menu', '' ); ?>
-		</header>		
+		</header>
 		<div id="page" class='page-wrapper'>
 			<div id="content" class="content clear">
 				<main>
-					<div class='post-wrapper'>		
+					<div class='post-wrapper'>
