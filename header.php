d907e9d55977ed51b257cf79a547490816fa17c9
Joe Dolson <design@joedolson.com>
2025-08-07 10:16:14 -0500

    Remove skiplinks

--- header.php
+++ header.php
@@ -8,19 +8,14 @@
 </head>
 <body <?php body_class(); ?>>
 	<div id="wrapper" class='wrapper'>
-		<header>	
-		<?php apply_filters( 'universal_before_header', '' ); ?>
+		<header>
 		<div id="header" class='header'>
-				<?php apply_filters( 'universal_top_of_header', '' ); ?>
 				<div class="text-header">
 					<div class='site-title'><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></div>
 					<div class='site-description'><?php bloginfo('description'); ?></div>
 				</div>
-				<?php apply_filters( 'universal_end_of_header', '' ); ?>	
 		</div>
-		<?php apply_filters( 'universal_before_primary_menu', '' ); ?>
-		<?php get_template_part( 'skiplinks' ); ?>
-		<div class='primary-menu'>		
+		<div class='primary-menu'>
 			<span class='menu-toggle' title="<?php _e( 'Menu','universal' ); ?>"></span>
 			<nav>
 			<?php wp_nav_menu( array( 'theme_location'=>'primary' ) ); ?>
